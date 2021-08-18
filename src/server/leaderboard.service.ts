88a922650cc82d6aa702bb04061a73ef6a2f9178
Nick Main <nickmaster24@gmail.com>
2021-08-17 22:15:17 -0400

    Refactored LeaderboardService

--- src/server/leaderboard.service.ts
+++ src/server/leaderboard.service.ts
@@ -1,4 +1,4 @@
-import { OnStart, Service } from '@rbxts/crochet';
+import { OnInit, OnStart, Service } from '@rbxts/crochet';
 
 const PlayerService = game.GetService('Players');
 
@@ -6,45 +6,55 @@ type Leaderstats = BoolValue & {
     'Seconds Alive': IntValue;
 };
 
-export class LeaderboardService extends Service implements OnStart {
-    public onStart(): void {
+export class LeaderboardService extends Service implements OnInit, OnStart {
+    private deltaElapsed = 0;
+
+    public onInit(): void {
         PlayerService.PlayerAdded.Connect((player) => {
-            const leaderstats = new Instance('BoolValue');
-            leaderstats.Name = 'leaderstats';
-            const secondsAlive = new Instance('IntValue');
-            secondsAlive.Name = 'Seconds Alive';
-            secondsAlive.Parent = leaderstats;
-
-            player.CharacterAdded.Connect((character) => {
-                (character.WaitForChild('Humanoid') as Humanoid).Died.Connect(() => {
-                    secondsAlive.Value = 0;
-                });
-            });
-
-            leaderstats.Parent = player;
+            this.createLeaderstatsForPlayer(player);
         });
+    }
 
-        function leaderstatsOfPlayer(player: Player): undefined | Leaderstats {
-            const leaderstats = player.FindFirstChild('leaderstats');
-            if (!leaderstats) {
-                return undefined;
-            }
-            return leaderstats as Leaderstats;
-        }
-
-        let elapsed = 0;
-
+    public onStart(): void {
         game.GetService('RunService').Stepped.Connect((time: number, delta: number) => {
-            elapsed += delta;
-            if (elapsed > 1) {
-                elapsed -= 1;
-                for (let player of PlayerService.GetPlayers()) {
-                    const leaderstats = leaderstatsOfPlayer(player);
-                    const alive = (player.Character?.FindFirstChild('Humanoid') as Humanoid)?.Health > 0;
-                    if (!leaderstats || !alive) continue;
-                    leaderstats['Seconds Alive'].Value += 1;
-                }
-            }
+            this.onStepped(delta);
         });
+    }
+
+    public getLeaderstatsOfPlayer(player: Player): undefined | Leaderstats {
+        const leaderstats = player.FindFirstChild('leaderstats');
+        if (!leaderstats) {
+            return undefined;
+        }
+        return leaderstats as Leaderstats;
+    }
+
+    private createLeaderstatsForPlayer(player: Player): void {
+        const leaderstats = new Instance('BoolValue');
+        leaderstats.Name = 'leaderstats';
+        const secondsAlive = new Instance('IntValue');
+        secondsAlive.Name = 'Seconds Alive';
+        secondsAlive.Parent = leaderstats;
+
+        player.CharacterAdded.Connect((character) => {
+            (character.WaitForChild('Humanoid') as Humanoid).Died.Connect(() => {
+                secondsAlive.Value = 0;
+            });
+        });
+
+        leaderstats.Parent = player;
+    }
+
+    private onStepped(delta: number): void {
+        this.deltaElapsed += delta;
+        if (this.deltaElapsed > 1) {
+            this.deltaElapsed -= 1;
+            for (let player of PlayerService.GetPlayers()) {
+                const leaderstats = this.getLeaderstatsOfPlayer(player);
+                const alive = (player.Character?.FindFirstChild('Humanoid') as Humanoid)?.Health > 0;
+                if (!leaderstats || !alive) continue;
+                leaderstats['Seconds Alive'].Value += 1;
+            }
+        }
     }
 }
